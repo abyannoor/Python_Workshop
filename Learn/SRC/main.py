763f957b4633088279153a536430b64adab2c23b
abyannoor <ariqabyan@gmail.com>
2022-11-22 11:14:17 +0700

    Change Data Type

---
+++ Learn/SRC/main.py
@@ -0,0 +1,19 @@
+# Casting
+# Change data type  to another type
+# data type = int, float, str, boolean
+
+data_int = 9;
+print("data = ", data_int, ",type =",type(data_int))
+
+# Integer to Float
+data_float = float(data_int)
+
+# Integer to String
+data_str = str(data_int)
+
+# Integer to Boolean
+data_bool  = bool(data_int) 
+ 
+print("data = ", data_float, ",type =",type(data_float))
+print("data = ", data_str, ",type =",type(data_str))
+print("data = ", data_bool, ",type =",type(data_bool))
