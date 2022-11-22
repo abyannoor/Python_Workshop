20a8f16581e007ff06d245602c76022c4b2c2a2e
abyannoor <ariqabyan@gmail.com>
2022-11-22 12:17:10 +0700

    Create Input Data

---
+++ 07_Mengambil_Input_Data_dari_User/SRC/main.py
@@ -0,0 +1,18 @@
+# Input User
+
+# input data is string
+#data = input("Masukkan Data: ")
+
+#print("data = ",data,",type = ",type(data)) # will be printed as string data
+
+
+# jika mau mengambil integer
+#angka = float(input("Masukkan Angka: "))
+#angka = int(input("Masukkan Angka: "))
+#print("data = ",angka,",type = ",type(angka)) # will be printed as string data
+
+
+
+# Input Data Boolean
+biner = bool(int(input("Masukkan Boolean: ")))
+print("data = ",biner,",type = ",type(biner))
